f1d364375758b7784adceaa4f3c0b626abf14a3f
Mark <m.delloro@yahoo.com>
2016-10-11 16:55:19 -0400

    controller changes

--- src/main/java/co/grandcircus/shindapp/controller/ItemController.java
+++ src/main/java/co/grandcircus/shindapp/controller/ItemController.java
@@ -2,28 +2,16 @@ package co.grandcircus.shindapp.controller;
 
 import java.util.Locale;
 
-
-import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-
-import co.grandcircus.shindapp.rest.ItemService;
-
 @Controller
 public class ItemController {
-	
-	@Autowired
-	private ItemService itemService;
 
-
+	@RequestMapping(value = "/item", method = RequestMethod.GET)
 	public String participantList(Locale locale, Model model) {
 		//logger.info("Welcome home! The client locale is {}.", locale);
-		
-		model.addAttribute("results", itemService.getItemInfoByName(itemService.getSessionID(ItemService.getKey()), "hummus", ItemService.getKey()));
 		
 		
 		
